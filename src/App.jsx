4ec7be45e69e12df352083033ee180b19d2931a6
Shajidaa <shajidaakterlopa101@gmail.com>
2025-10-03 20:20:44 +0600

    ui design

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function generateAllNewDice(){
 }
 //win the game 
 
-const gameWon=dice.every(die=>die.isHeld)&&
-dice.every(die=>die.value= dice[0].value)
+const gameWon=  dice.every(die=>die.isHeld)&&
+                dice.every(die=>die.value=== dice[0].value)
 
 
 
@@ -60,11 +60,14 @@ const diceElements=dice.map(dieObj=><Die
 
 
   return (
-    <main className='flex  flex-col 
-      justify-center 
-       items-center' >
-        {gameWon&& <ReactConfetti  ></ReactConfetti>}
-
+    <main className=' max-w-[400px] max-h-[400px] mx-auto' >
+        <div className='flex  flex-col 
+      justify-center  
+       items-center min-h-dvh' >
+     {gameWon?( <ReactConfetti  ></ReactConfetti>):''}
+        <h1 className='text-2xl font-bold'>Tenzies</h1>
+        <p className='text-xl text-center font-normal'>Roll until all dice are the same.
+           Click each die to freeze it at its current value between rolls.</p>
     <div className='dice-container grid grid-cols-5  gap-4 p-5'>
      {diceElements}
     </div>
@@ -73,7 +76,17 @@ const diceElements=dice.map(dieObj=><Die
        className='p-3
       rounded font-bold bg-blue-600
        text-white'>{ gameWon?"New Game" : "Roll"}</button>
+
+      {/* <p className={`text-xl  `}>
+        {
+        gameWon?  alert ("You won the game"):('keep Rolling  ')
+       }
+        
+        </p>  */}
+       
     </div>
+        </div>
+   
     </main> 
   )
 }
